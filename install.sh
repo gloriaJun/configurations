02082559c1aac7eea48ab2aacf3c380ec4ea8159
Gloria Jun <gloriajun@Gloriaui-MacBookPro.local>
2019-10-19 18:53:36 +0900

    Update configuration

--- install.sh
+++ install.sh
@@ -1,5 +1,8 @@
 # !/bin/bash
 
+clear
+mode=$1
+
 function _install() {
 	local dir=$1
 	
@@ -11,26 +14,40 @@ function _install() {
 	echo "######################################"
 }
 
+function _result() {
+	local result=$?
+	local target=$1
+
+	if [ $result -ne 0 ]; then
+		echo "---- $target Install Error ---"
+		exit -1;
+	fi
+
+	echo "######################################"
+	echo "======== $target Installed =========="
+	echo "######################################"
+}
+
 ## install brew
-#/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-#echo "######################################"
-#echo "======== Homebrew Installed =========="
-#echo "######################################"
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+_result Hombebrew
+
 
 # install via brew
-#brew bundle --file=./Brewfile
+brew bundle --file=./brew/Brewfile
+_result Brewfile 
+if [ $mode -eq "home" ];then
+	brew bundle --file=./brew/Brewfile_home
+	_result Brewfile_home
+fi
 sh ./openApp.sh
-
-echo "######################################"
-echo "===== Brewfile Package Installed ====="
-echo "######################################"
-
+_result openApp
 
 # configure iterm
 _install iterm2 
 
 # install zsh 
-_install xcode
+_install zsh 
 
 # install xcode
 _install xcode
---
+++ npm/install.sh
@@ -0,0 +1,1 @@
+yarn -g install npx
